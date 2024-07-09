46188e579a4a005219a10d7ef4e3bbbf392b41c3
DevRamona <r.ingabire@alustudent.com>
2024-07-09 16:12:39 +0200

    refactor

--- TypeScript/assert.js
+++ TypeScript/assert.js
@@ -1,13 +1,18 @@
-// // Type assertion using the tags 
-// let code : any = 123
-// let employeeCode = <number> code
-// console.log(typeof(employeeCode))
-// function myFunc(name : string) : void {
-// }
-// myFunc("Hello")
-// const hero : string = "Hello"
-// let nun : never
-// Using the as keyword 
-var cup = 124;
-var employeeCup = cup.toString().length;
-console.log(employeeCup);
+// // // Type assertion using the tags 
+// // let code : any = 123
+// // let employeeCode = <number> code
+// // console.log(typeof(employeeCode))
+// // function myFunc(name : string) : void {
+// // }
+// // myFunc("Hello")
+// // const hero : string = "Hello"
+// // let nun : never
+// // Using the as keyword 
+// const cup : unknown = 124;
+// const employeeCup = (cup as string).toString().length
+// console.log(employeeCup)
+var someValue = 123;
+var strLength1 = someValue.toFixed(3);
+var strLength2 = someValue.length;
+console.log(strLength1); // Output: 5 (length of "hello")
+console.log(strLength2); // Output: 5 (length of "hello")
--- TypeScript/assert.ts
+++ TypeScript/assert.ts
@@ -1,23 +1,31 @@
-// // Type assertion using the tags 
-// let code : any = 123
-// let employeeCode = <number> code
-// console.log(typeof(employeeCode))
+// // // Type assertion using the tags 
+// // let code : any = 123
+// // let employeeCode = <number> code
+// // console.log(typeof(employeeCode))
 
 
-// function myFunc(name : string) : void {
+// // function myFunc(name : string) : void {
      
 
-// }
-// myFunc("Hello")
+// // }
+// // myFunc("Hello")
 
-// const hero : string = "Hello"
-// let nun : never
+// // const hero : string = "Hello"
+// // let nun : never
 
 
-// Using the as keyword 
-const cup : unknown = 124;
-const employeeCup = (cup as string).toString().length
-console.log(employeeCup)
+// // Using the as keyword 
+// const cup : unknown = 124;
+// const employeeCup = (cup as string).toString().length
+// console.log(employeeCup)
+
+
+let someValue: unknown = "hello";
+let strLength1: number = (someValue as string).length;
+let strLength2: number = (<string>someValue).length;
+
+console.log(strLength1); // Output: 5 (length of "hello")
+console.log(strLength2); // Output: 5 (length of "hello")
 
 
 
