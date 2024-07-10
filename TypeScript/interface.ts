63560fc078b00b2a367a19b131015aacb87a49f6
DevRamona <r.ingabire@alustudent.com>
2024-07-10 09:38:02 +0200

    how to use interfaces

--- TypeScript/interface.ts
+++ TypeScript/interface.ts
@@ -1,45 +1,67 @@
-// interface animal {
-//     name : string,
-//     age : number, 
-//     makeSound() : void; 
+// // interface animal {
+// //     name : string,
+// //     age : number, 
+// //     makeSound() : void; 
 
+// // }
+
+// // class Dog implements animal {
+// //     constructor(name : string, age : number ) {
+// //         this.name = name, 
+// //         this.age = age;
+// //     }
+// //     makeSound() {
+// //         console.log(`${this.name} says woof!`);
+// //     }
+// // }
+// // const myDog = new Dog("Buddy", 3);
+// // myDog.makeSound(); // Output: "Buddy says woof!"
+
+// // Define the interface
+// interface Animal {
+//     name: string;
+//     age: number;
+//     makeSound(): void;
 // }
 
-// class Dog implements animal {
-//     constructor(name : string, age : number ) {
-//         this.name = name, 
+// // Implement the interface in the Dog class
+// class Dog implements Animal {
+//     name: string;
+//     age: number;
+
+//     constructor(name: string, age: number) {
+//         this.name = name;
 //         this.age = age;
 //     }
+
 //     makeSound() {
 //         console.log(`${this.name} says woof!`);
 //     }
 // }
+
+// // Create an instance of the Dog class and use it
 // const myDog = new Dog("Buddy", 3);
 // myDog.makeSound(); // Output: "Buddy says woof!"
 
-// Define the interface
-interface Animal {
-    name: string;
-    age: number;
-    makeSound(): void;
+
+interface Person {
+    name : string, 
+    age : number, 
+    occupation ? : string 
 }
 
-// Implement the interface in the Dog class
-class Dog implements Animal {
-    name: string;
-    age: number;
+const Person1 : Person = {
+    name : "John", 
+    age : 40
 
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
+} 
 
-    makeSound() {
-        console.log(`${this.name} says woof!`);
-    }
+const Person2 : Person = {
+    name : "John", 
+    age : 40, 
+    occupation : "Father"
+
 }
 
-// Create an instance of the Dog class and use it
-const myDog = new Dog("Buddy", 3);
-myDog.makeSound(); // Output: "Buddy says woof!"
-
+console.log(Person1.occupation)
+console.log(Person2.age)
--- TypeScript/unknown.ts
+++ TypeScript/unknown.ts
@@ -1,15 +1,21 @@
-const  userInput : unknown = 5
-let userName : string = "Hello"
+// const  userInput : unknown = 5
+// let userName : string = "Hello"
 
-// Type assertion to narrow down the type of userInput
-if(typeof userInput === "string") {
-    userName = userInput;
-}
+// // Type assertion to narrow down the type of userInput
+// if(typeof userInput === "string") {
+//     userName = userInput;
+// }
 
-function processInput(input:unknown) {
-    if(typeof input === "string") {
-        console.log(input.toUpperCase())
-    } else {
-        console.log("Input is not a string")
-    }
+// function processInput(input:unknown) {
+//     if(typeof input === "string") {
+//         console.log(input.toUpperCase())
+//     } else {
+//         console.log("Input is not a string")
+//     }
+// }
+
+const human : unknown = "person"
+if(typeof human === "string") {
+    console.log(`Done`)
 }
+console.log(human)
